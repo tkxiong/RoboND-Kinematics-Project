7b3097180f94a80586f1b65774323be6095dd3a2
kai xiong <legendarykx.kaixiong1@gmail.com>
2018-12-01 18:04:36 +0800

    final

--- IK_debug.py
+++ IK_debug.py
@@ -62,14 +62,114 @@ def test_code(test_case):
     ########################################################################################
     ## 
 
-    ## Insert IK code here!
+    # Define DH param symbols
+    d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8') # link offset
+    a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7') # link length
+    alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7') # twist angle
+
+    # Joint angle symbols
+    q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
+
+    # Create Modified DH parameters
+    dh = {alpha0:      0, a0:      0, d1:  0.75, q1:        q1,
+        alpha1: -pi/2., a1:   0.35, d2:     0, q2: -pi/2.+q2,
+        alpha2:      0, a2:   1.25, d3:     0, q3:        q3,
+        alpha3: -pi/2., a3: -0.054, d4:   1.5, q4:        q4,
+        alpha4:  pi/2., a4:      0, d5:     0, q5:        q5,
+        alpha5: -pi/2., a5:      0, d6:     0, q6:        q6,
+        alpha6:      0, a6:      0, d7: 0.303, q7:         0}
+
+    # Function to return homogeneous transform matrix
+
+    def TF_Mat(alpha, a, d, q):
+        TF = Matrix([[            cos(q),           -sin(q),           0,             a],
+                    [ sin(q)*cos(alpha), cos(q)*cos(alpha), -sin(alpha), -sin(alpha)*d],
+                    [ sin(q)*sin(alpha), cos(q)*sin(alpha),  cos(alpha),  cos(alpha)*d],
+                    [                 0,                 0,           0,             1]])
+        return TF
+
+    ## Substitute DH_Table
+    T0_1 = TF_Mat(alpha0, a0, d1, q1).subs(dh)
+    T1_2 = TF_Mat(alpha1, a1, d2, q2).subs(dh)
+    T2_3 = TF_Mat(alpha2, a2, d3, q3).subs(dh)
+    T3_4 = TF_Mat(alpha3, a3, d4, q4).subs(dh)
+    T4_5 = TF_Mat(alpha4, a4, d5, q5).subs(dh)
+    T5_6 = TF_Mat(alpha5, a5, d6, q6).subs(dh)
+    T6_7 = TF_Mat(alpha6, a6, d7, q7).subs(dh)
+
+    T0_ee = T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_7
+
+    # Extract end-effector position and orientation from request
+    # px, py, pz = end-effector position
+    # roll, pitch, yaw = end-effector orientation
+    px = req.poses[x].position.x
+    py = req.poses[x].position.y
+    pz = req.poses[x].position.z
+
+    (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
+        [req.poses[x].orientation.x, req.poses[x].orientation.y,
+        req.poses[x].orientation.z, req.poses[x].orientation.w]
+    )
+
+    # Find EE rotation matrix
+    # Define RPY rotation matrices
+    # http://planning.cs.uiuc.edu/node102.html
+
+    r, p, y = symbols('r p y')
+
+    ROT_x = Matrix([[1, 0, 0],
+                    [0, cos(r), -sin(r)],
+                    [0, sin(r), cos(r)]]) # Roll
     
-    theta1 = 0
-    theta2 = 0
-    theta3 = 0
-    theta4 = 0
-    theta5 = 0
-    theta6 = 0
+    ROT_y = Matrix([[cos(p), 0, sin(p)],
+                    [0, 1, 0],
+                    [-sin(p), 0, cos(p)]]) # Pitch
+
+    ROT_z = Matrix([[cos(y), -sin(y), 0],
+                    [sin(y), cos(y), 0],
+                    [0, 0, 1]]) # Yaw
+
+    ROT_ee = ROT_z * ROT_y * ROT_x
+
+    # More information can be found in KR210 Forward Kinematics section
+    Rot_Error = ROT_z.subs(y, radians(180)) * ROT_y.subs(p, radians(-90))
+    
+    ROT_ee = ROT_ee * Rot_Error
+    ROT_ee = ROT_ee.subs({'r': roll, 'p': pitch, 'y': yaw})
+
+    ee = Matrix([[px],
+                [py],
+                [pz]])
+    
+
+    # Calculate wrist center
+    wc = ee - (0.303) * ROT_ee[:,2]
+
+    # Calculate joint
+    
+    theta1 = atan2(wc[1], wc[0])
+
+    # SSS triangle for theta2 and theta3
+    side_a = 1.501
+    side_b = sqrt(pow((sqrt(wc[0] * wc[0] + wc[1] * wc[1]) - 0.35), 2) + pow((wc[2] - 0.75), 2))
+    side_c = 1.25
+
+    # Find each side of angle using cosine rule
+    angle_a = acos((side_b * side_b + side_c * side_c - side_a * side_a) / (2 * side_b * side_c))
+    angle_b = acos((side_a * side_a + side_c * side_c - side_b * side_b) / (2 * side_a * side_c))
+    angle_c = acos((side_a * side_a + side_b * side_b - side_c * side_c) / (2 * side_a * side_b)) 
+
+    theta2 = pi / 2 - angle_a - atan2(wc[2] - 0.75, sqrt(wc[0] * wc[0] + wc[1] * wc[1]) - 0.35)
+    theta3 = pi / 2 - (angle_b + 0.036) # 0.036 accounts for sag in link4 of -0.054m
+    
+    R0_3 = T0_1[0:3, 0:3] * T1_2[0:3, 0:3] * T2_3[0:3, 0:3]
+    R0_3 = R0_3.evalf(subs={q1: theta1, q2: theta2, q3: theta3})
+
+    R3_6 = R0_3.inv("LU") * ROT_ee
+    
+    theta4 = atan2(R3_6[2, 2], -R3_6[0, 2])
+    theta5 = atan2(sqrt(R3_6[0, 2] * R3_6[0, 2] + R3_6[2, 2] * R3_6[2, 2]), R3_6[1, 2])
+    theta6 = atan2(-R3_6[1, 1], R3_6[1, 0])
 
     ## 
     ########################################################################################
@@ -79,13 +179,14 @@ def test_code(test_case):
     ## as the input and output the position of your end effector as your_ee = [x,y,z]
 
     ## (OPTIONAL) YOUR CODE HERE!
-
+    fk = T0_ee.evalf(subs={q1: theta1, q2: theta2, q3: theta3, q4: theta4, q5: theta5, q6: theta6})
+    
     ## End your code input for forward kinematics here!
     ########################################################################################
 
     ## For error analysis please set the following variables of your WC location and EE location in the format of [x,y,z]
-    your_wc = [1,1,1] # <--- Load your calculated WC values in this array
-    your_ee = [1,1,1] # <--- Load your calculated end effector value from your forward kinematics
+    your_wc = [wc[0], wc[1], wc[2]] # <--- Load your calculated WC values in this array
+    your_ee = [fk[0, 3], fk[1, 3], fk[2, 3]] # <--- Load your calculated end effector value from your forward kinematics
     ########################################################################################
 
     ## Error analysis
--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 [![Udacity - Robotics NanoDegree Program](https://s3-us-west-1.amazonaws.com/udacity-robotics/Extra+Images/RoboND_flag.png)](https://www.udacity.com/robotics)
 # Robotic arm - Pick & Place project
 
+## KUKA KR210 Robotics Arm
+
+[//]: # (Image References)
+[image0]: ./misc_images/kuka_kr210.png
+[image1]: ./misc_images/individual_matrices_drawing.jpg
+[image2]: ./misc_images/joints_and_links_drawing.jpg
+![alt text][image0] 
+
 Make sure you are using robo-nd VM or have Ubuntu+ROS installed locally.
 
 ### One time Gazebo setup step:
@@ -74,28 +82,84 @@ If you are running in demo mode, this is all you need. To run your own Inverse K
 $ cd ~/catkin_ws/src/RoboND-Kinematics-Project/kuka_arm/scripts
 $ rosrun kuka_arm IK_server.py
 ```
-Once Gazebo and rviz are up and running, make sure you see following in the gazebo world:
 
-	- Robot
-	
-	- Shelf
-	
-	- Blue cylindrical target in one of the shelves
-	
-	- Dropbox right next to the robot
-	
+### Forward Kinematic Analysis
 
-If any of these items are missing, report as an issue.
+'kr210.urdf.xacro' specify the joints and links of the KR210 robotics arm.
 
-Once all these items are confirmed, open rviz window, hit Next button.
+No. | joint | parent | child | x | y | z | r | p | y |
+--- | --- | --- | --- | --- | --- | --- | --- | --- | --- |
+0 | fixed_base | base_footprint | base_link | 0 | 0 | 0 | 0 | 0 | 0 |
+1 | joint_1 | base_link | link_1 | 0 | 0 | 0.33 | 0 | 0 | 0 |
+2 | joint_2 | link_1 | link_2 | 0 .35| 0 | 0.42 | 0 | 0 | 0 |
+3 | joint_3 | link_2 | link_3 | 0 | 0 | 1.25 | 0 | 0 | 0 |
+4 | joint_4 | link_3 | link_4 | 0.96 | 0 | -0.054 | 0 | 0 | 0 |
+5 | joint_5 | link_4 | link_5 | 0.54 | 0 | 0 | 0 | 0 | 0 |
+6 | joint_6 | link_5 | link_6 | 0.193 | 0 | 0 | 0 | 0 | 0 |
+7 | gripper | link_6 | gripper_link | 0.11 | 0 | 0 | 0 | 0 | 0 |
+- | **Total (m)** | - | - | **2.153** | 0 | **1.946** | 0 | 0 | 0 |
 
-To view the complete demo keep hitting Next after previous action is completed successfully. 
+#### Specify the joints and links
 
-Since debugging is enabled, you should be able to see diagnostic output on various terminals that have popped up.
+![alt text][image2] 
 
-The demo ends when the robot arm reaches at the top of the drop location. 
+#### Kuka KR210 robotics arm DH paramters
 
-There is no loopback implemented yet, so you need to close all the terminal windows in order to restart.
+Links | i | alpha(i-1) | a(i-1) | d(i) | theta(i) |
+:---: | :---: | :---: | :---: | :---: | :---: |
+0->1 | 1 | 0 | 0 | 0.75 | q1 |
+1->2 | 2 | -90 | 0.35 | 0 | -90+q2 |
+2->3 | 3 | 0 | 0 | 1.25 | q3 |
+3->4 | 4 | -90 | -0.0054 | 1.5 | q4 |
+4->5 | 5 | 90 | 0 | 0 | q5 |
+5->6 | 6 | -90 | 0 | 0 | q6 |
+6->7 | 7 | 0 | 0 | 0.303 | q7 |
 
-In case the demo fails, close all three terminal windows and rerun the script.
+#### Individual Transform Matrices
 
+![alt text][image1] 
+
+### Inverse Kinematics
+
+#### Joint 1 to 3
+
+```python
+theta1 = atan2(wc[1], wc[0]) 
+```
+
+```python
+# SSS triangle for theta2 and theta3
+side_a = 1.501
+side_b = sqrt(pow((sqrt(wc[0] * wc[0] + wc[1] * wc[1]) - 0.35), 2) + pow((wc[2] - 0.75), 2))
+side_c = 1.25
+
+# Find each side of angle using cosine rule
+angle_a = acos((side_b * side_b + side_c * side_c - side_a * side_a) / (2 * side_b * side_c))
+angle_b = acos((side_a * side_a + side_c * side_c - side_b * side_b) / (2 * side_a * side_c))
+angle_c = acos((side_a * side_a + side_b * side_b - side_c * side_c) / (2 * side_a * side_b)) 
+
+theta2 = pi / 2 - angle_a - atan2(wc[2] - 0.75, sqrt(wc[0] * wc[0] + wc[1] * wc[1]) - 0.35)
+```
+
+```python
+theta3 = pi / 2 - (angle_b + 0.036) # 0.036 accounts for sag in link4 of -0.054m
+```
+
+#### Joint 4 to 6
+```python
+R0_3 = T0_1[0:3, 0:3] * T1_2[0:3, 0:3] * T2_3[0:3, 0:3]
+R0_3 = R0_3.evalf(subs={q1: theta1, q2: theta2, q3: theta3})
+
+R3_6 = R0_3.inv("LU") * ROT_ee
+
+theta4 = atan2(R3_6[2, 2], -R3_6[0, 2])
+theta5 = atan2(sqrt(R3_6[0, 2] * R3_6[0, 2] + R3_6[2, 2] * R3_6[2, 2]), R3_6[1, 2])
+theta6 = atan2(-R3_6[1, 1], R3_6[1, 0])
+```
+
+### Result
+
+
+<a href="https://youtu.be/Yn3yv6ZRrX4" target="_blank"><img src="misc_images/result.png" alt="Final Result" width="480" height="320" border="10" /></a>
+
+Video link
